278ab45e9bf37b24b4b8dfe879f04b43f79b625c
udons11 <110922856+udons11@users.noreply.github.com>
2022-08-10 00:00:57 +0300

    Update SpeedBoYS.lua

--- SpeedBoYS.lua
+++ SpeedBoYS.lua
@@ -1,3 +1,3 @@
 while game:GetService("RunService").RenderStepped:wait()
-game:GetService("Workspace").Players.LocalPlayer.Character.Humanoid.WalkSpeed = 200
+game:GetService("Workspace").Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000
 end
